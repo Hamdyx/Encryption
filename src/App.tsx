ba59f312b205d51b48d1ca141e88c0ad1f08799c
hamdyx <a.hamdy.nig@gmail.com>
2022-07-05 20:46:16 +0200

    add typescript

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+const App: React.FC = () => {
+	return (
+		<div className="App">
+			<div className="App-header">
+				<h2>Welcome to React</h2>
+			</div>
+			<p className="App-intro">
+				To get started, edit <code>src/App.js</code> and save to reload.
+			</p>
+		</div>
+	);
+};
+
+export default App;
