cfa347a8fff6a41f03b272bc4f1f8e85a2f7a6c2
hamdyx <a.hamdy.nig@gmail.com>
2022-08-20 18:47:17 +0200

    add react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,30 @@
 import React from 'react';
 import PasswordGenerator from './features/password-generator';
 import CaesarCipher from './features/caesar-cipher';
-
+import { BrowserRouter, Routes, Route, Link, NavLink } from 'react-router-dom';
 const App: React.FC = () => {
 	return (
 		<div className="App">
-			<PasswordGenerator />
-			<CaesarCipher />
+			<BrowserRouter>
+				<nav className="navbar">
+					<NavLink to="/">Home</NavLink>
+					<NavLink to="/generator">Generator</NavLink>
+					<NavLink to="/cipher">Cipher</NavLink>
+				</nav>
+				<Routes>
+					<Route
+						path="/"
+						element={
+							<>
+								<PasswordGenerator />
+								<CaesarCipher />
+							</>
+						}
+					/>
+					<Route path="/generator" element={<PasswordGenerator />} />
+					<Route path="/cipher" element={<CaesarCipher />} />
+				</Routes>
+			</BrowserRouter>
 		</div>
 	);
 };
